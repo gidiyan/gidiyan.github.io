8320e2512482369f706e90348a0dc8b5baf6e673
gidiyan <gidiyan@gmail.com>
2020-10-12 17:43:40 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ class Storage {
     static getCart(){
         return localStorage.getItem('cart')?JSON.parse(localStorage.getItem('cart')):[];
     }
-    // static getProduct(id){
-    //     let products =JSON.parse(localStorage.getItem('products'));
-    //     return products.find(product => product.id === +(id));
-    // }
-    // static getProducts(){
-    //     return JSON.parse(localStorage.getItem("products"));
-    // }
+    static getProduct(id){
+        let products =JSON.parse(localStorage.getItem('products'));
+        return products.find(product => product.id === +(id));
+    }
+    static getProducts(){
+        return JSON.parse(localStorage.getItem("products"));
+    }
 }
 
 class Product {
@@ -26,33 +26,40 @@ class Product {
             const name = item.name;
             const price = item.price;
             const img = item.img;
-            return {id,name,price,img};
+            const category = item.category;
+            const subcategory = item.subcategory;
+            return {id,name,price,img,category,subcategory};
         });
     }
 }
 
-class App{
-    cart =[];
+class App {
+    cart = [];
     cartItems = document.querySelector('.cart-items');
-    clearCart= document.querySelector('.clear-cart');
+    clearCart = document.querySelector('.clear-cart');
     cartTotal = document.querySelector('.cart-total');
+    countItems = document.querySelector('.count-items-in-cart');
     sidebar = document.querySelector('.sidebar');
     constructor() {
         const closeBTN = document.querySelector('.close-btn');
         const show_cart = document.querySelector('.fa-dolly-flatbed');
         show_cart.addEventListener("click", () => this.opencart());
         closeBTN.addEventListener("click", () => this.closecart());
+        if (document.querySelector('.categoriesShow')) {
+            this.makeCategories(categories);
+        };
         this.makeShowcase(products);
         let data = new Product();
         Storage.saveProducts(data.getProducts());
         this.cart = Storage.getCart();
     }
-    getProduct = (id) => products.find(product => product.id===+(id));
+
+    getProduct = (id) => products.find(product => product.id === +(id));
     createProduct = (data) =>
         `
        <div class="product cs-1 md-2 tb-3 tb-4 pic-center pic-parent"  data-id="${data.id}">
            <img alt="${data.name}" src="${data.img}" class="img-fluid">
-           <strong class="sm-bottom prod-bottom">${data.price}</strong>
+           <strong class="sm-bottom prod-bottom font-size--small">${data.price}</strong>
            <strong class="sm-desc prod-desc">${data.name}</strong>
             <ul class="prod-text">
                <li><button class="but-like sm-button button"><i class="far fa-thumbs-up"></i></button></li>
@@ -60,32 +67,34 @@ class App{
                <li><button class="sm-button button"><i class="fas fa-info-circle"></i></button></li>
            </ul>
         </div>
-`       ;
-    makeShowcase(products)  {
+`;
+
+    makeShowcase(products) {
         let result = '';
-        products.forEach( item => {
-            result+=this.createProduct(item);
+        products.forEach(item => {
+            result += this.createProduct(item);
         });
         document.querySelector('.showcase').innerHTML = result;
     }
-    CreateCartItem(item){
-        const div=document.createElement('div');
+
+    CreateCartItem(item) {
+        const div = document.createElement('div');
         div.className = "cart-item";
-        div.setAttribute('id',item.id);
-        div.innerHTML=
+        div.setAttribute('id', 'id' + item.id);
+        div.innerHTML =
             `
                     <div class="picture product-img">
                         <img src="${item.img}" alt="${item.img}" class="img-fluid w-100">
                     </div>
                     <div class="product_name mx-auto">${item.name}</div>
                     <div class="remove-btn text-right">
-                        <a href="#" class="reset-anchor"><i id="trash" class="fas fa-trash-alt" data-id="${item.id}"></i></a>
+                        <a href="#" class="reset-anchor"><i id="trash" class="fas fa-trash-alt" data-id=${item.id}></i></a>
                     </div>
                     <div class="quantity">
                         <div class="border d-flex justify-content-around mx-auto">
-                            <i class="fas fa-caret-left" data-id="${item.id}"></i>
-                            <span id=cart_count class="border-1 p-1 amount" data-id="${item.amount}">1</span>
-                            <i class="fas fa-caret-right" data-id="${item.id}"></i>
+                            <i class="fas fa-caret-left" data-id=${item.id}></i>
+                            <span id=cart_count class="border-1 p-1 amount">${item.amount}</span>
+                            <i class="fas fa-caret-right" data-id=${item.id}></i>
                         </div>
                     </div>
                     <div class="price m-auto">
@@ -96,109 +105,120 @@ class App{
     `
         this.cartItems.append(div);
     }
-    addProductToCart(item){
-        const addToCartButtons = [...document.querySelectorAll('.but-cart')];
 
-        addToCartButtons.forEach( button => {
-            const  countItemsInCart = document.querySelector('.count-items-in-cart');
+    addProductToCart(item) {
+        const addToCartButtons = [...document.querySelectorAll('.but-cart')];
+        addToCartButtons.forEach(button => {
+            const countItemsInCart = document.querySelector('.count-items-in-cart');
             button.addEventListener('click', event => {
-                if(event.target.classList.contains('cart_pressed')) {}
-                 else {
-                    let cartItem = { ...this.getProduct(event.target.closest('.product').getAttribute('data-id')), amount: 1 };
-                    this.cart = [...this.cart,cartItem];
+                // let product = this.getProduct(event.target.closest('.product').getAttribute('data-id'));
+                // let incart = this.cart.some(elem => elem.id === product.id);
+                // console.log(product);
+                // console.log(incart);
+                // if(incart){
+                //     this.cart.forEach(elem => {
+                //         if(elem.id === product.id){
+                //             elem.amount += 1;
+                //         }
+                //     })}
+                // else
+                    if (event.target.classList.contains('cart_pressed')) {} else {
+                    let cartItem = {
+                        ...this.getProduct(event.target.closest('.product').getAttribute('data-id')),
+                        amount: 1
+                    };
+                    this.cart = [...this.cart, cartItem];
                     this.CreateCartItem(cartItem);
                     event.target.classList.add('cart_pressed');
                     event.target.innerText = "Added to cart";
                     +countItemsInCart.textContent++;
-                    if (+(countItemsInCart.textContent)>0){
+                    if (+(countItemsInCart.textContent) > 0) {
                         countItemsInCart.classList.add('notempty');
                     } else {
                         countItemsInCart.classList.remove('notempty');
                         countItemsInCart.innerHTML = " ";
                     }
                     Storage.saveCart(this.cart);
-                    this.setcartTotal(this.cart)
-                }});
+                }
+            });
         });
     }
+
     clearcart() {
         this.cart = [];
-        while(this.cartItems.children.length>0){
+        while (this.cartItems.children.length > 0) {
             this.cartItems.removeChild(this.cartItems.children[0]);
         }
         Storage.saveCart(this.cart);
         this.setcartTotal(this.cart);
     }
-    filterItem = (cart,currentItem) => cart.filter(item => item.id !== +(currentItem.dataset.id));
-    findItem = (cart,findingItem) => cart.find(item =>item.id === +(findingItem.dataset.id));
-    actualNavCartCount (cartId,toClear) {
-        const  countItemsInCart = document.querySelector('.count-items-in-cart');
-        for (let i=0;i<toClear.length;i++) {
-            if (toClear.item(i).dataset.id === cartId) {
-                this.createProduct(cartId);
+
+    filterItem = (cart, currentItem) => cart.filter(item => item.id !== +(currentItem.dataset.id));
+    findItem = (cart, findingItem) => cart.find(item => item.id === +(findingItem.dataset.id));
+
+    actualNavCartCount(cartId) {
+        const toClear = document.querySelectorAll('.product');
+        const countItemsInCart = document.querySelector('.count-items-in-cart');
+        for (let i = 0; i < toClear.length; i++) {
+            if (+(toClear.item(i).getAttribute('data-id')) === +(cartId.substr(2,2))) {
+                this.createProduct(cartId.substr(2,2));
                 let y = document.querySelectorAll('.cart_pressed');
-                for (let i=0;i<y.length;i++) {
-                    if (y[i].parentElement.parentElement.parentElement.parentElement.dataset.id === cartId){
-                        y[i].innerText = "ADD TO CART";
+                console.dir(y);
+                for (let i = 0; i < y.length; i++) {
+                    if (+(y[i].parentElement.parentElement.parentElement.parentElement.dataset.id) === +(cartId.substr(2,2))) {
+                        y[i].textContent = "ADD TO CART";
                         y[i].classList.remove('cart_pressed');
                         +countItemsInCart.textContent--;
-                        if (+(countItemsInCart.textContent)>0){
+                        if (+(countItemsInCart.textContent) > 0) {
                             countItemsInCart.classList.add('notempty');
                         } else {
                             countItemsInCart.classList.remove('notempty');
                             countItemsInCart.innerHTML = "";
                         }
-                        this.setcartTotal(this.cart)
+                        this.setcartTotal(this.cart);
+                        Storage.saveCart(this.cart);
                     }
                 }
             }
         }
     }
-    renderCart() {
-        this.clearCart.addEventListener('click',() => this.clearcart());
-        this.cartItems.addEventListener('click',event => {
 
-            if(event.target.classList.contains('fa-trash-alt')){
-                this.cart = this.filterItem(this.cart,event.target);
+    renderCart() {
+        this.clearCart.addEventListener('click', () => this.clearcart());
+        this.cartItems.addEventListener('click', event => {
+            if (event.target.classList.contains('fa-trash-alt')) {
+                this.cart = this.filterItem(this.cart, event.target);
+                event.target.closest('.cart-item').remove();
                 Storage.saveCart(this.cart);
-                this.setcartTotal(this.cart);
-                this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);
-                let cartId= event.target.parentElement.parentElement.parentElement.getAttribute('id');
-                let toClear = document.querySelectorAll('.product');
-                this.actualNavCartCount(cartId,toClear);
-            }
-            else if(event.target.classList.contains('fa-caret-right')){
+                let cartId = event.target.parentElement.parentElement.parentElement.getAttribute('id');
+                this.actualNavCartCount(cartId);
+            } else if (event.target.classList.contains('fa-caret-right')) {
                 let tmp = this.findItem(this.cart, event.target);
-                tmp.amount = tmp.amount+1;
+                tmp.amount = tmp.amount + 1;
                 event.target.previousElementSibling.innerText = tmp.amount;
                 this.setcartTotal(this.cart);
                 Storage.saveCart(this.cart);
-                this.setcartTotal(this.cart);
 
-            }
-            else if(event.target.classList.contains('fa-caret-left')){
+            } else if (event.target.classList.contains('fa-caret-left')) {
                 let tmp = this.findItem(this.cart, event.target);
-                tmp.amount =tmp.amount-1;
-                if(tmp.amount>0) {
+                if (tmp !== undefined && tmp.amount > 1) {
+                    tmp.amount = tmp.amount - 1;
                     event.target.nextElementSibling.innerText = tmp.amount;
                 } else {
-                    this.cart = this.filterItem(this.cart,event.target);
-                    this.cartItems.removeChild(event.target.parentElement.parentElement.parentElement);
-                    let cartId= event.target.parentElement.parentElement.parentElement.getAttribute('id');
-                    let toClear = document.querySelectorAll('.product');
-                    this.actualNavCartCount(cartId,toClear);
+                    this.cart = this.filterItem(this.cart, event.target);
+                    event.target.closest('.cart-item').remove();
+                    let cartId = event.target.parentElement.parentElement.parentElement.getAttribute('id');
+                    this.actualNavCartCount(cartId);
                 }
                 Storage.saveCart(this.cart);
                 this.setcartTotal(this.cart);
             }
-
-
-
         })
     }
-    testlike () {
+
+    testlike() {
         const testlike = [...document.querySelectorAll('.but-like')];
-        const likeMe=document.querySelector('.like_me');
+        const likeMe = document.querySelector('.like_me');
         testlike.forEach(button => {
             button.addEventListener('click', likeevent => {
                 if (likeevent.target.classList.contains('btn_pressed')) {
@@ -217,37 +237,126 @@ class App{
             })
         })
     }
+
     setcartTotal(cart) {
-        let  itemsInCart = document.querySelectorAll('.cart-item');
-        // let totalInCart = document.querySelector('.cart-total');
-        for (let item of itemsInCart){
-            const price = item.querySelector('.product_price').textContent;
-            let quantity = item.querySelector('.amount').textContent;
-            let countsub = quantity*price;
-            item.querySelector('.product-subtotal').textContent = parseFloat(countsub.toFixed(2));
-        }
-        let countTotal =0;
-        let itemsTotal =0;
+
+        let tmpTotal = 0;
         cart.map(item => {
-            countTotal+=item.price*item.amount;
-            itemsTotal+=item.amount;
+            tmpTotal = item.price * item.amount;
+            this.cartItems.querySelector(`#id${item.id} .product-subtotal`).textContent = parseFloat(tmpTotal.toFixed(2));
         });
-        this.cartTotal.textContent = parseFloat(countTotal.toFixed(2));
+        this.cartTotal.textContent = parseFloat(cart.reduce((previous, current) => previous + current.price * current.amount, 0).toFixed(2));
+        this.countItems.textContent = cart.reduce((previous,currnet) => previous+currnet.amount,0);
     }
-    opencart(){
+
+    opencart() {
         this.sidebar.classList.toggle("show-sidebar");
         document.querySelector('.over').classList.add('active')
+        this.cartItems.innerHTML='';
+        this.cart = Storage.getCart();
         this.populateCart(this.cart);
-    };
-    closecart(){
+        this.setcartTotal(this.cart);
+    }
+
+    closecart() {
         this.sidebar.classList.remove("show-sidebar");
         document.querySelector('.over').classList.remove('active');
     }
 
+    populateCart(cart) {
+        cart.forEach(item => this.CreateCartItem(item));
+    }
+
+    createCategory(category) {
+        return `
+         <a class="cat-overflow" data-category="${category.name}" href="#"><img alt="categories block" src="${category.img}" class="img-fluid transform"><strong class="text-inside1 category-item" data-category="${category.name}">"${category.name}"</strong></a>
+        `
+    }
+
+    makeCategories(categories) {
+        for (let i = 0; i < 3; i++) {
+            let div = document.createElement('div');
+            div.className = "cs-3 tb-3";
+            div.innerHTML = this.createCategory(categories[i]);
+            document.querySelector('.categoriesShow').append(div);
+        }
+    }
+
+    categoriesList() {
+        if (document.querySelector('.categories-list')) {
+            let result = ``;
+            subcategories.forEach(element => {
+                result += `<li class="incol mb-2 mt-3"><a class="reset_anchor  subcategory-item" href="#" data-subcategory="${element.name}">${element.name}</a></li>`;
+            });
+            document.querySelector('.categories-list').innerHTML = result;
+        }
+    }
+
+    renderCategory() {
+        const categories = document.querySelector('.categories');
+        categories.addEventListener('click', (event) => {
+            const target = event.target;
+            if (target.classList.contains('category-item')) {
+                const category = target.dataset.category;
+                const categoryFilter = items => items.filter(item => item.category.includes(category));
+                this.makeShowcase(categoryFilter(Storage.getProducts()));
+            } else if (
+                target.classList.contains('subcategory-item')) {
+                const subcategory = target.dataset.subcategory;
+                const categoryFilter = items => items.filter(item => item.subcategory.includes(subcategory));
+                this.makeShowcase(categoryFilter(Storage.getProducts()));
+            } else {
+                const categoryFilter = items => items.filter(item => item.category.includes(category));
+                this.makeShowcase((Storage.getProducts()));
+            }
+            this.addProductToCart();
+            this.renderCart();
+        });
+    }
+
+    selectPicker() {
+        if (document.querySelector('.selectpicker')) {
+            let selectpicker = document.querySelector('.selectpicker')
+            selectpicker.addEventListener('change', () => {
+                switch (selectpicker.value) {
+                    case 'low-high':
+                        this.makeShowcase(Storage.getProducts().sort(this.compareValue('price', 'asc')));
+                        break;
+                    case 'high-low':
+                        this.makeShowcase(Storage.getProducts().sort(this.compareValue('price', 'desc')));
+                        break;
+                    case 'popularity':
+                        this.makeShowcase(Storage.getProducts().sort(this.compareValue('id', 'asc')));
+                        break;
+                    default:
+                        this.makeShowcase(Storage.getProducts().sort(this.compareValue('id', 'asc')));
+                        break;
+                }
+            });
+        }
+
+
+    }
+
+    compareValue(key,order = 'asc'){
+        return function innerSort(a,b) {
+            if(!a.hasOwnProperty(key) || (!b.hasOwnProperty(key))) {
+                return 0;
+            }
+            const varA= (typeof a[key] === 'string') ? a[key].toUpperCase():a[key];
+            const varB= (typeof b[key] === 'string') ? b[key].toUpperCase():b[key];
+            let comparison =0;
+            if (varA>varB){comparison=1;} else if (varA<varB) {comparison=-1;} return ((order==='desc')?(comparison*(-1)):comparison);
+        }
+    };
 }
+
 (function(){
     const app = new App();
     app.addProductToCart();
     app.renderCart();
     app.testlike();
+    app.renderCategory();
+    app.categoriesList();
+    app.selectPicker();
 })();
--- js/data.js
+++ js/data.js
@@ -3,47 +3,106 @@ const products=[
         id:1,
         img:"images/prod1.jpg",
         name:"Origen Regional Red",
-        price:26.99
+        price:26.99,
+        category:"Food",
+        subcategory:"Kibble/Dry"
     },
     {
         id:2,
         img:"images/prod2.jpg",
-        name:"Origen Regional Red",
-        price:27.99
+        name:"Grandorf Brown",
+        price:27.99,
+        category: "Food",
+        subcategory:"Kibble/Dry"
     },
     {
         id:3,
         img:"images/prod3.jpg",
-        name:"Origen Regional Red",
-        price:28.99
+        name:"Carnilove Carps&Trout",
+        price:28.99,
+        category: "Food",
+        subcategory:"Canned"
     },
     {
         id:4,
         img:"images/prod4.jpg",
-        name:"Origen Regional Red",
-        price:29.99
+        name:"Earthborn Coastal Catch",
+        price:29.99,
+        category: "Food",
+        subcategory: "Canned"
     },
     {
         id:5,
         img:"images/prod5.jpg",
-        name:"Origen Regional Red",
-        price:30.99
+        name:"Carnilove Salmon&Turkey",
+        price:30.99,
+        category: "Food",
+        subcategory: "Semi-Moist"
     },
     {
         id:6,
         img:"images/prod6.jpg",
-        name:"Origen Regional Red",
-        price:31.99
+        name:"Moschino Hoody",
+        price:31.99,
+        category: "Wear",
+        subcategory: "Wear"
     },
     {
         id:7,
         img:"images/prod7.jpg",
-        name:"Origen Regional Red",
-        price:32.99
+        name:"Grey Suite",
+        price:32.99,
+        category: "Wear",
+        subcategory: "Wear"
     }, {
         id:8,
         img:"images/prod8.jpg",
-        name:"Origen Regional Red",
-        price:33.99
+        name:"Smart Toy",
+        price:33.99,
+        category: "Toys",
+        subcategory: "Toys"
     },
 ];
+const categories =[
+    {
+        id:1,
+        img:"images/food.jpg",
+        name:"Food"
+    },
+    {
+        id:2,
+        img:"images/wear.jpg",
+        name:"Wear"
+    },
+    {
+        id:3,
+        img:"images/toys.jpg",
+        name:"Toys"
+    }];
+    const subcategories =[
+    {
+        id:1,
+        img:"images/toys.jpg",
+        name:"Semi-Moist"
+    },
+    {
+        id:2,
+        img:"images/toys.jpg",
+        name:"Canned"
+    },
+    {
+        id:3,
+        img:"images/toys.jpg",
+        name:"Kibble/Dry"
+    },
+    {
+        id:4,
+        img:"images/wear.jpg",
+        name:"Wear"
+    },
+    {
+        id:5,
+        img:"images/toys.jpg",
+        name:"Toys"
+     }
+]
